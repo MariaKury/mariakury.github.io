66042befe612c2471b8e9f01648f8d6b60f2e4e6
Ian Kury <iankwen@gmail.com>
2020-12-20 15:21:28 -0300

    create VS solution

--- script.js
+++ script.js
@@ -3,18 +3,14 @@
 //   e.preventDefault()
 // })
 
-
-
 $(document).ready(Main)
 
 function Main() {
   $(window).on('resize', Resize_Handler)
 
-  Resize_Handler() 
+  Resize_Handler()
 
 }
-
-
 
 function Resize_Handler() {
   $(window).off('scroll')
@@ -22,27 +18,22 @@ function Resize_Handler() {
     $(window).on('scroll', Scroll_Handler)
 }
 
-
-
 function Scroll_Handler() {
   Handle_Scroll()
   Scroll_Mail()
 }
 
 function Handle_Scroll() {
-	const SPEED = 0.50
-  const initial_y = -0.2 * window.innerHeight 
+  const SPEED = 0.50
+  const initial_y = -0.2 * window.innerHeight
   const new_y = SPEED * window.scrollY + initial_y
   $('.header_fon img').css('top', new_y)
 }
 
 function Scroll_Mail() {
-	const SPEED = 0.6
+  const SPEED = 0.6
   const initial_y = 0
   const top_boundary = $('.mail').offset().top
   const new_y = SPEED * (window.scrollY - top_boundary) + initial_y
   $('.mail_fon img').css('top', new_y)
 }
-
-
-
