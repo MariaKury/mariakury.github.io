deb718b6de08221c71c357eb7f8f55bd9790184e
Maria Kury <mariaskury@gmail.com>
2020-12-30 19:25:48 -0300

    new version

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-Prepare_Popup()
 
-const mobile = () => window.innerWidth < 769
+
+
+const mobile = () => window.innerWidth < 996
+const not_animation = () => window.innerWidth < 1200
 
 $(document).ready(Main)
 
@@ -11,40 +13,21 @@ function Main() {
     Set_Mobile_Menu()
 
   Set_Menu_Links()
-
-  if (mobile())
-    $('body').on('click', Body_Click_Handler)
-
   Resize_Handler()
-
-  $('.button_sale').on('click', Display_Popup)
-
-  $('#popup_sale').on('click', Close_Popup_Sale)
 
   $('#btn_send').on('click', Send_Handler)
 }
 
-function Prepare_Popup() {
-  $('.popup_content').slideUp(0)
-}
 
-function Display_Popup() {
-  $('#popup_sale').show()
-  $('.popup_content').slideDown(400)
-}
-
-function Close_Popup_Sale() {
-  $('.popup_content').slideUp(300, () => $('#popup_sale').hide())
-}
 
 function Resize_Handler() {
   $(window).off('scroll')
-  if (!mobile())
     $(window).on('scroll', Scroll_Handler)
 }
 
 function Scroll_Handler() {
   Scroll_Header()
+  Scroll_Header_Sub_fon()
   Scroll_Mail()
 }
 
@@ -55,67 +38,68 @@ function Scroll_Header() {
   $('.header_fon img').css('top', new_y)
 }
 
+
+function Scroll_Header_Sub_fon() {
+  const SPEED = 0.6
+  const initial_y = 0
+  const top_boundary = $('.sub_fon').offset().top
+  const new_y = SPEED * (window.scrollY - top_boundary) + initial_y
+  $('.sub_fon img').css('top', new_y)
+}
+
 function Scroll_Mail() {
   const SPEED = 0.6
   const initial_y = 0
-  const top_boundary = $('.mail').offset().top
+  const top_boundary = $('.mail_fon').offset().top
   const new_y = SPEED * (window.scrollY - top_boundary) + initial_y
   $('.mail_fon img').css('top', new_y)
 }
 
-function Body_Click_Handler(e) {
-  if ($('.menu').is(':visible')) 
-    Close_Menu()
-  else {
-    const daddy = document.querySelector('.banner_burger')
-    if ($.contains(daddy, e.target))
-      Open_Menu()
-  }
-}
-
-function Open_Menu() {
-  $('#popup_menu').show()
-  $('.open_burger_menu').hide()
-}
-
-function Close_Menu() {
-  $('#popup_menu').hide()
-  $('.open_burger_menu').show()
-}
 
 function Send_Handler() {
   $('.svg_send').css('display', 'inline-block')
   setTimeout(() => $('.svg_send').hide(), 3000)
 }
 
-const banner_h = () => $('#banner').get(0).offsetHeight
 
 function Set_Menu_Links() {
-  $('#btn_home').on('click', () =>
+  $('.btn_home').on('click', () =>
     $('body').animate({
       scrollTop: '0'
     }))
-  $('#btn_about').on('click', () =>
+  $('.btn_about').on('click', () =>
     $('body').animate({
-      scrollTop: $('.about').offset().top - banner_h()
+      scrollTop: $('.about').offset().top 
     }))
-  $('#btn_products').on('click', () =>
+  $('.btn_portfolio').on('click', () =>
     $('body').animate({
-      scrollTop: $('.products').offset().top - banner_h()
+      scrollTop: $('.portfolio').offset().top 
     }))
-  $('#btn_stores').on('click', () =>
+  $('.btn_stores').on('click', () =>
     $('body').animate({
-      scrollTop: $('.stores').offset().top - banner_h()
+      scrollTop: $('.stores').offset().top 
     }))
-  $('#btn_sales').on('click', () =>
+  $('.btn_products').on('click', () =>
     $('body').animate({
-      scrollTop: $('.sales').offset().top - banner_h()
+      scrollTop: $('.products').offset().top 
     }))
 }
 
+
 function Set_Mobile_Menu() {
-  const menu = $('.menu')
-  menu.remove()
-  $('.banner_row').append($('<div id="popup_menu" class="popup"></div>'))
-  $('#popup_menu').append(menu)
+  $('.open_burger_menu').on('click', OpenMenu)
+  $('.close_menu').on('click', CloseMenu)
+  $('.menu_row__link').on('click', CloseMenuFast)
+}
+
+ function OpenMenu() {
+  $('.menu').animate({'top': "0"}, 500);
+}
+
+ function CloseMenu() {
+  $('.menu').animate({'top': "-100vh"}, 500);
+}
+
+ function CloseMenuFast() {
+  $('.menu').animate({'top': "-100vh"}, 0);
 }
